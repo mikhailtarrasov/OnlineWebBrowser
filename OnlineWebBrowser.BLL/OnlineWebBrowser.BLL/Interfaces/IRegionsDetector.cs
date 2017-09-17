040d6c47949bc0251cd2e16ffbea05adf43b3d03
Михаил <mikhailtarrasov@gmail.com>
2017-09-17 23:47:43 +0500

    Реализовано распознавание лиц с помощью библиотеки Emgu CV

---
+++ OnlineWebBrowser.BLL/OnlineWebBrowser.BLL/Interfaces/IRegionsDetector.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Emgu.CV;
+
+namespace OnlineWebBrowser.BLL.Interfaces
+{
+    public interface IRegionsDetector
+    {
+        void Detect(Mat image, List<Rectangle> regions);
+    }
+}
---
+++ OnlineWebBrowser.BLL/OnlineWebBrowser.BLL/RegionsDetectors/FacesDetector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Cuda;
+using Emgu.CV.Structure;
+using OnlineWebBrowser.BLL.Interfaces;
+
+namespace OnlineWebBrowser.BLL.RegionsDetectors
+{
+    public class FacesDetector : IRegionsDetector
+    {
+        public CascadeClassifier Face { get; set; }
+
+        public FacesDetector(string haarcascade)
+        {
+            Face = new CascadeClassifier(haarcascade);
+        }
+
+        public void Detect(Mat image, List<Rectangle> faces)
+        {
+            var img = image.ToImage<Gray, Byte>();
+            faces.AddRange(Face.DetectMultiScale(img, 1.1, 3));
+        }
+    }
+}
